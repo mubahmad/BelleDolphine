fc06ac2a9e39865a57de0fc5fbf8c3344f484520
MaaitaMohammad <almaaitamohammad@gmail.com>
2024-05-12 23:36:47 +0300

    change write logs to append mode

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo nmcli device wifi hotspot ssid dolphin password 12344321 ifname wlan0
-/usr/bin/python3 ./flask_app/app.py &> /dev/null
-/usr/bin/python3 ./main.py &> /dev/null
+/usr/bin/python3 ./flask_app/app.py &>> /dev/null
+/usr/bin/python3 ./main.py &>> /dev/null
