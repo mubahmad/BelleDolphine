a3649ced3c08027ac94884cfe39ead345dab69bc
MaaitaMohammad <almaaitamohammad@gmail.com>
2024-05-14 18:55:35 +0300

    flask server progress

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 sudo nmcli device wifi hotspot ssid dolphin password 12344321 ifname wlan0
 echo problem1
-sudo /usr/bin/python3 ./flask_app/app.py &>> /dev/null
+sudo /usr/bin/python3 ./flask_app/app.py &>> /tmp/flask.log
 echo problem2
-/usr/bin/python3 ./main.py &>> /dev/null
+/usr/bin/python3 ./main.py &>> /tmp/dolphin.log
 echo problem3
