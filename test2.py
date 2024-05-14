7efa221fa56ce19a9c8cff35ee9a09fbc6cf553e
mubaidin <mubaidinahmad@gmail.com>
2024-05-14 20:08:43 +0300

    made the first test scenario

--- test2.py
+++ test2.py
@@ -5,8 +5,8 @@ from accessories import suck, sweep
 try:
     while True:
         # suck(True, 100)
-        # sweep(True, 70)
-        clean(True, True, True, 20)
+        sweep(True, 70)
+        # clean(True, True, True, 20)
 except KeyboardInterrupt:
     print("Program terminated")
 finally:
